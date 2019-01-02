c42c89d392527fee00f2d39021293e744388c3d7
Jason Basuil <basuilj@gmail.com>
2019-01-02 04:42:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
-# your code goes here
+def begins_with_r(array)
+  for i in array do 
+    if i.start_with?("r") == false 
+      return false 
+    end
+  end 
+  true 
+end 
+
+def contain_a(array)
+  contains_a = []
+  for i in array do 
+    if i.include?("a") == true 
+      contains_a << i 
+    end 
+  end 
+  contains_a
+end 
+
+def first_wa(array) 
+  wa = array.find do |item|
+    if item.respond_to?("chars")
+      item.downcase.start_with?("wa")
+    end 
+  end 
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |x|
+    x.class.to_s.downcase != "string"
+  end 
+end 
+
+def count_elements(array)
+  uniq_items = array.uniq
+  uniq_items.map do |item|
+    {:count => array.count(item), :name => item[:name]}
+  end
+end
+    
+def merge_data(keys, data)
+  merged_data = []
+  
+end 
+
+    
+  
+  
